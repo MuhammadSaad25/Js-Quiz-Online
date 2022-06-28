60e08710d1dc3d7e973e51f4ee84a34acbf75f92
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-06-28 17:14:02 +0500

    create files for Js online quiz app

--- app.js
+++ app.js
@@ -1,67 +1,7 @@
 
-let first_input = "";
-let second_input = "";
-
-let matrix1 = []
-let matrix2 = []
 
 function start() {
-    let matrix_input1 = document.querySelector("#matrix_input1");
-    let matrix_input2 = document.querySelector("#matrix_input2");
-    let sum_input = document.querySelector("#sum_input");
-    let sum_input1 = document.querySelector("#sum_input1");
-
-    matrix_input1.innerHTML = "";
-    matrix_input2.innerHTML = "";
-    sum_input.innerHTML = "";
-    sum_input1.innerHTML = "";
-
-    first_input = Number(document.querySelector("#first_input").value);
-    second_input = Number(document.querySelector("#second_input").value);
-
-    if (first_input == "" || second_input == "") {
-        alert("Please enter values");
-        return;
-    }
-
-    if (isNaN(first_input) || isNaN(second_input)) {
-        alert("Please enter numbers");
-        return;
-    }
-
-    if (first_input != second_input) {
-        alert("Please enter same number  of rows and columns");
-        return;
-    }
-
-    if (first_input < 2 || first_input < 2) {
-        alert("Please enter number greater than 1");
-        return;
-    }
-
    
-
-    for(let i = 0; i < first_input; i++) {
-        matrix1.push([]);
-        matrix2.push([]);
-        for(let j = 0; j < second_input; j++){
-            matrix1[i].push(0)
-            matrix2[i].push(0)
-            
-            matrix_input1.innerHTML += `<input type="number" class="matrix_input" id="matrix_input1_${i}_${j}" required value= "0" min = "0" max = "999">`;
-            
-            matrix_input2.innerHTML += `<input type="number" class="matrix_input" id="matrix_input2_${i}_${j}" required value= "0" min = "0" max = "999">` ;
-        
-            sum_input.innerHTML += `<input type="number" readonly class="matrix_input" id="sum_input_${i}_${j}" required placeholder ="0" >`;
-            
-            // sum_input1.innerHTML += `<input type="number" readonly class="matrix_input" id="sum_input1_${i}_${j}" required placeholder ="0" >`;
-        }
-
-        matrix_input1.innerHTML += `<br>`;
-        matrix_input2.innerHTML += `<br>`;
-        sum_input.innerHTML += `<br>`;
-    }
-    console.log (matrix1 , matrix2)    
     document.querySelector("#input_form").classList.remove("form_input");
 }
 
